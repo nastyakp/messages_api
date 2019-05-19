fbe5986c3002ab763cb549cbe5b5a5646495a936
Nastya Akopova <akopova_anastasia@mail.ru>
2019-05-19 22:09:04 +0300

    user tests green. added messages model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   include DeviseTokenAuth::Concerns::User
+
+  validates_presence_of :nickname
+  has_many :messages, dependent: :destroy
+
 end
